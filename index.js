b4246a02b08d56f594a364649ffb4b499a943f10
Nick Landas <nick.landas@gmail.com>
2022-11-01 10:03:04 -0500

    commit tree 7fa1ab0744151c8621a5cc53d5571cdf8c2a8d5b

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    //console.log(cats);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    //console.log(cats);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    //console.log(cats);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    //console.log(cats);
+}
+
+function appendCat(name){
+    return [...cats, name];
+    //console.log([...cats, name]);
+    //console.log(cats);
+}
+
+function prependCat(name){
+
+    return [name, ...cats];
+    //console.log([name, ...cats]);
+    //console.log(cats);
+}
+
+function removeLastCat(){
+   return cats.slice(0,cats.length - 1);
+   //console.log(cats.slice(0,cats.length - 1));
+   //console.log(cats);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+    //console.log(cats.slice(1));
+   //console.log(cats);
+}
